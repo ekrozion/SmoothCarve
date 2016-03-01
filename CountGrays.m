dc8b70f70bdab79b015450e50a6628e1d7d0aa3f
koswix <ben.alderson1@gmail.com>
2016-03-01 18:54:48 +0000

    Code tidy up

--- CountGrays.m
+++ CountGrays.m
@@ -1,11 +1,10 @@
-function [ handles ] = CountGrays( hObject, handles )
-%CountGrays Count the number of unique gray values
-%Count the number of grays in the converted image and store in the handles
-%structure
-handles.NumberGrays = unique(handles.InputArray);
-handles.NumberGrays = size(handles.NumberGrays);
-handles.NumberGrays = handles.NumberGrays(1,1);
-
-guidata(hObject,handles);
+function [handles] = CountGrays(hObject, handles)
+    % CountGrays Count the number of unique gray values
+    % Count the number of grays in the converted image and store in the handles
+    % structure
+    handles.NumberGrays = unique(handles.InputArray);
+    handles.NumberGrays = size(handles.NumberGrays);
+    handles.NumberGrays = handles.NumberGrays(1, 1);
+ 
+   guidata(hObject, handles);
 end
-
