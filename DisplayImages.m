2de35af9a6aa72b473bc49d6a59f46f1d87d2ffb
koswix <ben.alderson1@gmail.com>
2016-03-01 18:55:20 +0000

    Code tidy up

--- DisplayImages.m
+++ DisplayImages.m
@@ -1,28 +1,23 @@
-function [ handles ] = DisplayImages(hObject, handles)
-%DisplayImages 
-%   Displays the original image, converts to gray scale and displays that
-%   too
-
-%Select the input file axes and displays the unaltered image
-
-
-
-
-%Check if the image is colour RGB and converts to grayscale if required
-if ndims(handles.InputArray)>2
-    handles.InputArray = rgb2gray(handles.InputArray);
+function [handles] = DisplayImages(hObject, handles)
+    %DisplayImages
+    %   Displays the original image, converts to gray scale and displays that
+    %   too
+ 
+    %Select the input file axes and displays the unaltered image
+ 
+    %Check if the image is colour RGB and converts to grayscale if required
+    if ndims(handles.InputArray) > 2
+        handles.InputArray = rgb2gray(handles.InputArray);
+    end
+ 
+    %call the function to count the grays
+    handles = CountGrays (hObject, handles);
+ 
+    %Select the output file axes and display the converted file (or original
+    %file if conversion not required)
+    axes(handles.OutputAxes);
+    imshow(handles.InputArray);
+ 
+    %make sure all the data is written back to the handles structure
+    guidata(hObject, handles);
 end
-
-%call the function to count the grays
-handles = CountGrays (hObject, handles);
-
-
-%Select the output file axes and display the converted file (or original
-%file if conversion not required)
-axes(handles.OutputAxes);
-imshow(handles.InputArray);
-
-%make sure all the data is written back to the handles structure
-guidata(hObject,handles);
-end
-
