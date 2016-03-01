77a7fc8f008a728eb68557882dbe9a99747e2559
koswix <ben.alderson1@gmail.com>
2016-03-01 19:00:42 +0000

    Code tidy up

--- OutputSize.m
+++ OutputSize.m
@@ -1,15 +1,14 @@
-function [ handles ] = OutputSize( hObject,handles )
-%OutputSize Calculates the size of the engaving
-%  Multiplies the number of pixels (in X and Y) by the pixel size as
-%  specified by the user. GetUserData must be called before this function.
-
-outputX = handles.ImageDim(1,2) * handles.PixelSize;
-outputY = handles.ImageDim(1,1) * handles.PixelSize;
-
-%Saves the output size string into the handles structure. Display is
-%carriedout by UpdateText.m
-handles.OutputSize = ['Size (mm) : ' num2str(outputX) ' x ' num2str(outputY)];
-
-guidata(hObject,handles);
+function [handles] = OutputSize(hObject, handles)
+    %OutputSize Calculates the size of the engaving
+    %  Multiplies the number of pixels (in X and Y) by the pixel size as
+    %  specified by the user. GetUserData must be called before this function.
+ 
+    outputX = handles.ImageDim(1, 2) * handles.PixelSize;
+    outputY = handles.ImageDim(1, 1) * handles.PixelSize;
+ 
+    %Saves the output size string into the handles structure. Display is
+    %carriedout by UpdateText.m
+    handles.OutputSize = ['Size (mm) : ' num2str(outputX) ' x ' num2str(outputY)];
+ 
+    guidata(hObject, handles);
 end
-
