b215b49d82debd22dac9968bb95d6e06941245c2
koswix <ben.alderson1@gmail.com>
2016-03-01 19:01:44 +0000

    Code tidy up

--- PreJobCode.m
+++ PreJobCode.m
@@ -1,10 +1,9 @@
-function [ handles ] = PreJobCode( hObject,handles )
-%PreJobCodes The codes required before the job starts (homing, laser on,
-%whatever you need)
-%   Seperate codes with \n and finish with \n
-
-fprintf(handles.fileID,'G21\nG90\nM3\n');
-
-
+function [handles] = PreJobCode(hObject, handles)
+    %PreJobCodes The codes required before the job starts (homing, laser on,
+    %whatever you need)
+    %   Seperate codes with \n and finish with \n
+ 
+    fprintf(handles.fileID, 'G21\nG90\nM3\n');
+ 
 end
 
