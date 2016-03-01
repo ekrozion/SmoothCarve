6c7d34e00ac49c8b0a87f7685fa8308864797a29
koswix <ben.alderson1@gmail.com>
2016-03-01 19:01:14 +0000

    Code tidy up

--- PostJobCode.m
+++ PostJobCode.m
@@ -1,10 +1,8 @@
-function [ handles ] = PostJobCode( hObject,handles )
-%PostJobCode The codes required after the job starts (homing, laser off,
-%whatever you need)
-%   Seperate codes with \n and finish with \n
-
-fprintf(handles.fileID,'M5\nG90\nG0 X0 Y0\n');
-
-
+function [handles] = PostJobCode(hObject, handles)
+    %PostJobCode The codes required after the job starts (homing, laser off,
+    %whatever you need)
+    %   Seperate codes with \n and finish with \n
+ 
+    fprintf(handles.fileID, 'M5\nG90\nG0 X0 Y0\n');
+ 
 end
-
