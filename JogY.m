1650a32a82c14097d0a429a0f22ccd16e8acc549
koswix <ben.alderson1@gmail.com>
2016-03-01 18:58:02 +0000

    Code tidy up

--- JogY.m
+++ JogY.m
@@ -1,9 +1,8 @@
-function [ handles ] = JogY( hObject,handles )
-%JogY Move the Y direction
-%   Jogs the Y axis down by one pixel width at the end of each line
-
-Jog = handles.PixelSize;
-Jog = num2str(Jog);
-fprintf(handles.fileID,'G0 Y%s\n',Jog);
+function [handles] = JogY(hObject, handles)
+    %JogY Move the Y direction
+    %   Jogs the Y axis down by one pixel width at the end of each line
+ 
+    Jog = handles.PixelSize;
+    Jog = num2str(Jog);
+    fprintf(handles.fileID, 'G0 Y%s\n', Jog);
 end
-
