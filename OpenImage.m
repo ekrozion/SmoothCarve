35f6036a52938415a848bfd7fbe92aaa83cea013
koswix <ben.alderson1@gmail.com>
2016-03-01 18:59:38 +0000

    Code tidy up

--- OpenImage.m
+++ OpenImage.m
@@ -1,15 +1,14 @@
 function [handles] = OpenFile(hObject, handles)
-%OpenFile opens the UI file open dialoge
-%   Selects the file and stores the path and filename, and image data in
-%   the handles structure
-[filename, pathname] = uigetfile({'*.png';'*.jpg';'*.bmp';'*.*'});
-handles.FileName = strcat(pathname, filename);
-handles.InputArray = imread(handles.FileName);
-
-%Read the size of the image file
-handles.ImageDim = size(handles.InputArray);
-
-%update the handles structure with any canges
-guidata(hObject,handles);
+    %OpenFile opens the UI file open dialoge
+    %   Selects the file and stores the path and filename, and image data in
+    %   the handles structure
+    [filename, pathname] = uigetfile({'*.png'; '*.jpg'; '*.bmp'; '*.*'});
+    handles.FileName = strcat(pathname, filename);
+    handles.InputArray = imread(handles.FileName);
+ 
+    %Read the size of the image file
+    handles.ImageDim = size(handles.InputArray);
+ 
+    %update the handles structure with any canges
+    guidata(hObject, handles);
 end
-
