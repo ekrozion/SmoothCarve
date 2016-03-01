1561ba00ef5c9f7a8476e916992f5af66479197d
koswix <ben.alderson1@gmail.com>
2016-03-01 19:02:42 +0000

    Code tidy up

--- UpdateText.m
+++ UpdateText.m
@@ -1,20 +1,17 @@
-function [ handles ] = UpdateText(hObject,handles)
-%UdateText Update the various text on the gui
-
-
-%Create the string with the image size in it and dispaly in the GUI
-ImSize = ['Image Size: ' num2str(handles.ImageDim(1,2)) ' x ' num2str(handles.ImageDim(1,1))];
-set(handles.textImageSize, 'String', ImSize)  
-
-
-%Create the string with the number of grays in it and display in th GUI
-NumGrays = ['Number of grays: ' num2str(handles.NumberGrays)];
-set(handles.textNumberGrays, 'String', NumGrays);
-
-%Update output size
-set(handles.textOutputSize, 'String', handles.OutputSize);
-
-%update the handles structure with any canges
-guidata(hObject,handles);
+function [handles] = UpdateText(hObject, handles)
+    %UdateText Update the various text on the gui
+ 
+    %Create the string with the image size in it and dispaly in the GUI
+    ImSize = ['Image Size: ' num2str(handles.ImageDim(1, 2)) ' x ' num2str(handles.ImageDim(1, 1))];
+    set(handles.textImageSize, 'String', ImSize)
+ 
+    %Create the string with the number of grays in it and display in th GUI
+    NumGrays = ['Number of grays: ' num2str(handles.NumberGrays)];
+    set(handles.textNumberGrays, 'String', NumGrays);
+ 
+    %Update output size
+    set(handles.textOutputSize, 'String', handles.OutputSize);
+ 
+    %update the handles structure with any canges
+    guidata(hObject, handles);
 end
-
